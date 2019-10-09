bedd0aa124ea1d5870e8a59c09f4968b1b77be32
Igor Pysarenko <38840933+NothingToSayHB@users.noreply.github.com>
2019-10-09 20:45:14 +0300

    Update sessions.php

--- includes/sessions.php
+++ includes/sessions.php
@@ -1,21 +1,19 @@
 <?php
 
-/*Файл где стартует сессия*/
 
 session_start();
 
-//ф-я сообщение об ошибке
+
 function error_massage() {
     if (isset($_SESSION['ErrorMassage'])) {
         $out = "<div class=\"alert alert-danger\">";
-        $out .= htmlentities($_SESSION["ErrorMassage"]); // htmlentities преобразует все символы в сущности хтмл
+        $out .= htmlentities($_SESSION["ErrorMassage"]); 
         $out .= "</div>";
-        $_SESSION['ErrorMassage'] = null; // очищаем сессию чтобы при перезаргрузке не оставалось сообщение
+        $_SESSION['ErrorMassage'] = null; 
         return $out;
     }
 }
 
-//ф-я сообщение об успешном действии
 function success_massage() {
     if (isset($_SESSION['SuccessMassage'])) {
         $out = "<div class=\"alert alert-success\">";
